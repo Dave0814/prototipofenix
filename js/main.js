d868f8206aaa1199228593443a1e0e3bfa8e7548
David <david.e.morales1408@gmail.com>
2021-10-24 21:07:21 -0500

    se agregaron las tres vistas del menu desplegable, falta hacer el diseño de la pagina de cada una

--- js/main.js
+++ js/main.js
@@ -1,9 +1,3 @@
-/*window.addEventListener('load', function(){
-    new Glider(document.querySelector('.glider'), {
-      setting-name: setting-value
-    })
-  })*/
-
   var glider = new Glider(document.getElementById('glider'), {
     slidesToShow: 2,
   slidesToScroll: 1,
@@ -14,20 +8,3 @@
     next: '.glider-next'
   }
 });
-  
-  /*glider.setOption({
-    arrows: {
-      prev: '#glider-prev',
-      next: '#glider-next'
-    }
-  });
-  
-  // a refresh may need to be called depending
-  // upon the options passed to setOptions
-  glider.refresh(true);
-  
-  // adding or removing items automatically calls refresh()
-  //glider.removeItem(4);
-  
-  // death to smoochy!
-  //glider.destroy();*/
