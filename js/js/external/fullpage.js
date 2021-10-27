495e1c0f8ed0c4892d6b2f97ee074a216f609571
David <david.e.morales1408@gmail.com>
2021-10-26 21:54:03 -0500

    Avance de la seccion de 'nosotros'

---
+++ js/js/external/fullpage.js
@@ -0,0 +1,6 @@
+<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/fullPage.js/3.0.4/fullpage.extensions.min.js"></script>
+
+<script></script>
+$(document).ready(function() {
+ $('#fullpage').fullpage();
+});
